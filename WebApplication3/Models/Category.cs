03a9d781f0adac1cb622a11210f66417834dfe91
Haris Asadi <Haris Asadi@DESKTOP-I9ORGB6>
2024-05-23 17:36:18 +0500

    Add project files.

---
+++ WebApplication3/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebExpense.Models
+{
+    public class Categorycs
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public String Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";
+
+
+
+
+
+
+    }
+}
